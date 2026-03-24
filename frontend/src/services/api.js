bff522984599f8726e1f2b45d0f0e5e1cc3473e5
hetgajera15-star <hetgajera15@gmail.com>
2026-03-24 22:30:05 +0530

    het

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://home-rental-vrbh.onrender.com/api',
+  baseURL: 'http://localhost:5000/api',
 });
 
 API.interceptors.request.use((req) => {
