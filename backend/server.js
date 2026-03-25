886ff745f73c944ec103bd1eed234ae0a3b02f13
hetgajera15-star <hetgajera15@gmail.com>
2026-03-25 13:18:03 +0530

    dhhd

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
-require("dotenv").config();
+require("dotenv").config({ path: require('path').resolve(__dirname, '.env') }); // ✅ Change karo
 
 const app = express();
 
